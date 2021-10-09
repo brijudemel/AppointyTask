feca70e7883597f934c28182276439243340eba9
brijudemel <brijudemel@gmail.com>
2021-10-09 22:08:09 +0530

    final commit

---
+++ hw.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"context"
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type User struct {
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name     string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email    string             `json:"email,omitempty" bson:"email,omitempty"`
+	Password string             `json:"password,omitempty" bson:"password,omitempty"`
+}
+
+type Post struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserID    primitive.ObjectID `json:"userid,omitempty" bson:"userid,omitempty"`
+	Caption   string             `json:"caption,omitempty" bson:"caption,omitempty"`
+	ImageURL  string             `json:"imageurl,omitempty" bson:"imageurl,omitempty"`
+	Timestamp string             `json:"timestamp,omitempty" bson:"timestamp,omitempty"`
+}
+
+var client *mongo.Client
+
+// To create a user
+func CreateUserEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	var user User
+	json.NewDecoder(request.Body).Decode(&user)
+	collection := client.Database("Instagram").Collection("Users")
+	user.Password = fmt.Sprint(md5.Sum([]byte(user.Password)))
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	result, _ := collection.InsertOne(ctx, user)
+	json.NewEncoder(response).Encode(result)
+}
+
+//To get user of a specified id
+func GetUserEndPoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var user User
+	collection := client.Database("Instagram").Collection("Users")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := collection.FindOne(ctx, User{ID: id}).Decode(&user)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":"` + err.Error() + `"}`))
+		return
+	}
+	json.NewEncoder(response).Encode(user)
+}
+
+//To create a post
+func CreatePostEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	var post Post
+	json.NewDecoder(request.Body).Decode(&post)
+	collection := client.Database("Instagram").Collection("Posts")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	result, _ := collection.InsertOne(ctx, post)
+	json.NewEncoder(response).Encode(result)
+}
+
+//To get post of a specified id
+func GetPostEndPoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var post Post
+	collection := client.Database("Instagram").Collection("Posts")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := collection.FindOne(ctx, Post{ID: id}).Decode(&post)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":"` + err.Error() + `"}`))
+		return
+	}
+	json.NewEncoder(response).Encode(post)
+}
+
+//To get posts of a particular user whose user id is provided in parameters
+//Another parameter :items is passed to provide pagination. This has a number which define the number of posts to return
+func GetUserPostEndPoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	items, err := strconv.Atoi(params["items"])
+	var posts []Post
+	collection := client.Database("Instagram").Collection("Posts")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	cursor, err := collection.Find(ctx, Post{UserID: id})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":"` + err.Error() + `"}`))
+		return
+	}
+	var i int
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) && i < items {
+		var post Post
+		cursor.Decode(&post)
+		posts = append(posts, post)
+		i++
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"message":"` + err.Error() + `"}`))
+		return
+	}
+	json.NewEncoder(response).Encode(posts)
+}
+
+func main() {
+	fmt.Println("Start....")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	router := mux.NewRouter()
+	router.HandleFunc("/users", CreateUserEndpoint).Methods("POST")
+	router.HandleFunc("/users/{id}", GetUserEndPoint).Methods("GET")
+	router.HandleFunc("/posts", CreatePostEndpoint).Methods("POST")
+	router.HandleFunc("/posts/{id}", GetPostEndPoint).Methods("GET")
+	router.HandleFunc("/posts/users/{id}/{items}", GetUserPostEndPoint).Methods("GET")
+	http.ListenAndServe(":5001", router)
+
+}
